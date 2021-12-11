f07b189a1f9c95795d3e9fb1aa79d96b33874874
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-12-10 22:54:08 -0600

    SeguridadEntity

---
+++ WebApp/Entity/SeguridadEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    class SeguridadEntity:DBEntity
+    {
+        public int? IdSeguridad { get; set; }
+        public string NombreUsuario { get; set; }
+
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+
+
+    }
+}
