8bd6463ff05c11df83d8d65edcc970e1e4376330
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-12-10 22:57:36 -0600

    ClientesEntity

---
+++ WebApp/Entity/ClientesEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    class ClientesEntity : DBEntity
+
+    {
+        public int? IdCliente { get; set; }
+        public int? Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+    }
+}
